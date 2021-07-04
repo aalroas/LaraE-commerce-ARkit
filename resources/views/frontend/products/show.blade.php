3240aa23bd07bf79c261dc6c295679ff6c4f822d
aalroas <mr.a.alroas@gmail.com>
2021-07-04 17:05:20 +0300

    update product image view

--- resources/views/frontend/products/show.blade.php
+++ resources/views/frontend/products/show.blade.php
@@ -33,7 +33,7 @@
                                     </i>
                                 </button>
                             </div>
-                            <img src="{{asset($product->image)}}" style="width: 100%;height:50%;background-size: 100% 100%;"
+                            <img src="{{asset($product->image)}}" style="width: 100%;height:auto;background-size: 100% 100%;"
 
                                  class="  blur-up lazyload image_zoom_cls-0"></div>
 

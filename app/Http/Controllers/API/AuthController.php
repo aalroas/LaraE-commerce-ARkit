f9aeaf9abcc7db0de9c34d0580c44dd3079e77fc
aalroas <mr.a.alroas@gmail.com>
2021-07-07 14:02:06 +0300

    update api

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
